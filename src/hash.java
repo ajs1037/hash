5a0b836006071e73e06a261b2d941ef058ec5070
Adolfo Segura <adolfo.segura20@outlook.com>
2017-07-18 12:14:20 -0700

    Update hash.java

--- src/hash.java
+++ src/hash.java
@@ -1,104 +1,84 @@
-// hash.java
-// demonstrates hash table with linear probing
-// to run this program: C:>java HashTableApp
+// 
+// Demonstrates hash table with linear probing
+//
 import java.io.*;
-////////////////////////////////////////////////////////////////
-class DataItem
-   {                                // (could have more data)
-   private int iData;               // data item (key)
-//--------------------------------------------------------------
-   public DataItem(int ii)          // constructor
-      { iData = ii; }
-//--------------------------------------------------------------
-   public int getKey()
-      { return iData; }
-//--------------------------------------------------------------
-   }  // end class DataItem
-////////////////////////////////////////////////////////////////
-class HashTable
-   {
-   private DataItem[] hashArray;    // array holds hash table
+
+class DataItem {
+   private int iData;
+   
+   public DataItem(int ii) { 
+      iData = ii; 
+   }
+   public int getKey() {
+      return iData; 
+   }
+}
+
+class HashTable {
+   private DataItem[] hashArray;
    private int arraySize;
-   private DataItem nonItem;        // for deleted items
-// -------------------------------------------------------------
-   public HashTable(int size)       // constructor
-      {
+   private DataItem nonItem;
+
+   public HashTable(int size) {
       arraySize = size;
       hashArray = new DataItem[arraySize];
       nonItem = new DataItem(-1);   // deleted item key is -1
-      }
-// -------------------------------------------------------------
-   public void displayTable()
-      {
+   }
+
+   public void displayTable() {
       System.out.print("Table: ");
-      for(int j=0; j<arraySize; j++)
-         {
+      for(int j=0; j<arraySize; j++) {
          if(hashArray[j] != null)
             System.out.print(hashArray[j].getKey() + " ");
          else
             System.out.print("** ");
          }
       System.out.println("");
-      }
-// -------------------------------------------------------------
-   public int hashFunc(int key)
-      {
+   }
+
+   public int hashFunc(int key) {
       return key % arraySize;       // hash function
-      }
-// -------------------------------------------------------------
-   public void insert(DataItem item) // insert a DataItem
-   // (assumes table not full)
-      {
+   }
+   
+   public void insert(DataItem item) {
       int key = item.getKey();      // extract key
       int hashVal = hashFunc(key);  // hash the key
                                     // until empty cell or -1,
-      while(hashArray[hashVal] != null &&
-                      hashArray[hashVal].getKey() != -1)
-         {
+      while(hashArray[hashVal] != null &  hashArray[hashVal].getKey() != -1) {
          ++hashVal;                 // go to next cell
          hashVal %= arraySize;      // wraparound if necessary
-         }
+      }
       hashArray[hashVal] = item;    // insert item
-      }  // end insert()
-// -------------------------------------------------------------
-   public DataItem delete(int key)  // delete a DataItem
-      {
+   }
+   public DataItem delete(int key) {
       int hashVal = hashFunc(key);  // hash the key
 
-      while(hashArray[hashVal] != null)  // until empty cell,
-         {                               // found the key?
-         if(hashArray[hashVal].getKey() == key)
-            {
+      while(hashArray[hashVal] != null) {
+         if(hashArray[hashVal].getKey() == key) {
             DataItem temp = hashArray[hashVal]; // save item
             hashArray[hashVal] = nonItem;       // delete item
             return temp;                        // return item
-            }
+         }
          ++hashVal;                 // go to next cell
          hashVal %= arraySize;      // wraparound if necessary
-         }
+      }
       return null;                  // can't find item
-      }  // end delete()
-// -------------------------------------------------------------
-   public DataItem find(int key)    // find item with key
-      {
+  }  // end delete()
+
+   public DataItem find(int key) {
       int hashVal = hashFunc(key);  // hash the key
 
-      while(hashArray[hashVal] != null)  // until empty cell,
-         {                               // found the key?
+      while(hashArray[hashVal] != null) { 
          if(hashArray[hashVal].getKey() == key)
             return hashArray[hashVal];   // yes, return item
          ++hashVal;                 // go to next cell
          hashVal %= arraySize;      // wraparound if necessary
-         }
-      return null;                  // can't find item
       }
-// -------------------------------------------------------------
-   }  // end class HashTable
-////////////////////////////////////////////////////////////////
-class HashTableApp
-   {
-   public static void main(String[] args) throws IOException
-      {
+      return null;                  // can't find item
+   }
+}
+class HashTableApp {
+   public static void main(String[] args) throws IOException {
       DataItem aDataItem;
       int aKey, size, n, keysPerCell;
                                     // get sizes
@@ -110,21 +90,17 @@ class HashTableApp
                                     // make table
       HashTable theHashTable = new HashTable(size);
 
-      for(int j=0; j<n; j++)        // insert data
-         {
-         aKey = (int)(java.lang.Math.random() *
-                                         keysPerCell * size);
+      for(int j=0; j<n; j++) {
+         aKey = (int)(java.lang.Math.random() * keysPerCell * size);
          aDataItem = new DataItem(aKey);
          theHashTable.insert(aDataItem);
-         }
+      }
 
-      while(true)                   // interact with user
-         {
+      while(true) {
          System.out.print("Enter first letter of ");
          System.out.print("show, insert, delete, or find: ");
          char choice = getChar();
-         switch(choice)
-            {
+         switch(choice) {
             case 's':
                theHashTable.displayTable();
                break;
@@ -143,39 +119,32 @@ class HashTableApp
                System.out.print("Enter key value to find: ");
                aKey = getInt();
                aDataItem = theHashTable.find(aKey);
-               if(aDataItem != null)
-                  {
+               if(aDataItem != null) {
                   System.out.println("Found " + aKey);
-                  }
+               }
                else
                   System.out.println("Could not find " + aKey);
                break;
             default:
                System.out.print("Invalid entry\n");
-            }  // end switch
-         }  // end while
-      }  // end main()
-//--------------------------------------------------------------
-   public static String getString() throws IOException
-      {
+         }
+      }
+   }
+
+   public static String getString() throws IOException {
       InputStreamReader isr = new InputStreamReader(System.in);
       BufferedReader br = new BufferedReader(isr);
       String s = br.readLine();
       return s;
-      }
-//--------------------------------------------------------------
-   public static char getChar() throws IOException
-      {
+   }
+
+   public static char getChar() throws IOException {
       String s = getString();
       return s.charAt(0);
-      }
-//-------------------------------------------------------------
-   public static int getInt() throws IOException
-      {
+   }
+   public static int getInt() throws IOException {
       String s = getString();
       return Integer.parseInt(s);
-      }
-//--------------------------------------------------------------
-   }  // end class HashTableApp
-////////////////////////////////////////////////////////////////
+   }
+}
 
